77818b61feae045a4551aefe434901dc8275b43a
Evelios <twaters117@gmail.com>
2017-03-04 16:43:02 -0500

    SL2 and SignExtend

--- SignExtend.vhd
+++ SignExtend.vhd
@@ -8,7 +8,7 @@ use ieee.std_logic_arith.all;
 entity ShiftLeft2 is
 
 Port(
-	I:	in  std_logic_vector(31 downto 0);
+	I:	in  std_logic_vector(15 downto 0);
 	O:	out std_logic_vector(31 downto 0);
 );
 
@@ -19,6 +19,7 @@ end ShiftLeft2;
 Architecture behv of ShiftLeft2 is
 begin
 
-	O <= I sll 2;
+	O(31 downto 16) <= '0';
+	O(15 downto 0) <= I(15 downto 0);
 
 end behv;
