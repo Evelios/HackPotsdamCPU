ec6700ead077c024cec0cf5a6e1cb7335cdec5b2
Timyoshi <kilmertw@clarkson.edu>
2017-03-04 19:27:02 -0500

    fix ALU and implment overflow

--- ALU.vhd
+++ ALU.vhd
@@ -7,7 +7,8 @@ ENTITY ALU IS
           A          : IN     std_logic_vector(31 downto 0);--A is a 32 bit signal.
           B          : IN     std_logic_vector(31 downto 0);--B is a 32 bit signal.
           OPP        : IN     std_logic_vector(5 downto 0); --OPP specifies the operation to be acted on both A and B
-          Carry      : OUT    std_logic_vector(31 downto 0);--Carry out
+          CarryOut   : OUT    std_logic;--Carry out
+          Overflow   : OUT    std_logic;--Overflow
           Result     : OUT    std_logic_vector(31 downto 0) -- Result of A OPP B
         );
 END ALU;
@@ -21,21 +22,27 @@ PROCESS(A,B,OPP)
       CASE OPP IS
           WHEN '000000' =>
             Result <= A AND B;--AND
-
+            Overflow <= 0;
           WHEN '000001' =>
             Result <= A OR B;--OR
-
+            Overflow <= 0;
           WHEN '000010' =>
             Result <= A + B;--ADD
-
+            Overflow <= ((A(31) AND B(31)) AND (NOT Result))OR(((NOT A(31)) AND (NOT B(31))) AND Result));
           WHEN '000110' =>
             Result <= A - B; --SUB
-
+            Overflow <= 0;
           WHEN '000111' =>
             Result <= A < B;--Set on Less Than
-
+            Overflow <= 0;
           WHEN '001100'=>
             Result <= A NOR B;--NOR
+            Overflow <= 0;
+          WHEN OTHERS =>
+            Result <= A;-- Pass A
+            Overflow <= 0;
       END CASE;
 END PROCESS;
+
+
 END ALU_main;
