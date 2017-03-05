1cb14926c9cb1831bfa41527aa0083a152fba8a2
Tim <kilmertw@clarkson.edu>
2017-03-05 01:30:34 -0500

    Stuff and things for fpgas

--- SignExtend.vhd
+++ SignExtend.vhd
@@ -5,21 +5,21 @@ use ieee.std_logic_arith.all;
 
 ---------------------------------------------------
 
-entity ShiftLeft2 is
+entity SignExtend is
 
 Port(
 	I:	in  std_logic_vector(15 downto 0);
-	O:	out std_logic_vector(31 downto 0);
+	O:	out std_logic_vector(31 downto 0)
 );
 
-end ShiftLeft2;
+end SignExtend;
 
 ---------------------------------------------------
 
-Architecture behv of ShiftLeft2 is
+Architecture behv of SignExtend is
 begin
 
-	O(31 downto 16) <= '0';
+	O(31 downto 16) <= "0";
 	O(15 downto 0) <= I(15 downto 0);
 
 end behv;
